1ce49f4f6da6a883038464cff56a1b43c59bc163
Evan Byrne <evantbyrne@gmail.com>
2019-07-05 13:07:55 -0400

    Source maps and watching for changes on development builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,61 @@ const path = require("path");
 const MiniCssExtract = require("mini-css-extract-plugin");
 const Uglify = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: {
-    bundle: [
-      "./frontend/js/index.js",
-      "./frontend/scss/index.scss",
-    ],
-  },
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      bundle: [
+        "./frontend/js/index.js",
+        "./frontend/scss/index.scss",
+      ],
+    },
 
-  output: {
-    path: path.resolve(__dirname, 'frontend/dist'),
-    filename: "bundle.js",
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [
-          /node_modules/
-        ],
-        use: [
-          {
-            loader: "babel-loader"
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtract.loader,
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-    ],
-  },
-
-  plugins: [
-    new MiniCssExtract({
+    output: {
       path: path.resolve(__dirname, 'frontend/dist'),
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new Uglify(),
-  ],
+      filename: "bundle.js",
+    },
+
+    devtool: argv["mode"] === "development" ? "source-map" : false,
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: [
+            /node_modules/
+          ],
+          use: [
+            {
+              loader: "babel-loader"
+            },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtract.loader,
+            },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "sass-loader",
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtract({
+        path: path.resolve(__dirname, 'frontend/dist'),
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new Uglify({
+        sourceMap: argv["mode"] === "development",
+      }),
+    ],
+  };
 };
